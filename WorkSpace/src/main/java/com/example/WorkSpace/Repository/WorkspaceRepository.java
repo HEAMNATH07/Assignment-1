fa99811723e4c9ea651a78c7777270972aac4428
HEAMNATH07 <heamnathnagarajan@gmail.com>
2025-01-23 09:25:21 +0530

    Delete WorkSpace directory

--- WorkSpace/src/main/java/com/example/WorkSpace/Repository/WorkspaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.WorkSpace.Repository;
-
-import com.example.WorkSpace.Model.Workspace;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkspaceRepository extends MongoRepository<Workspace, Long> {
-}
